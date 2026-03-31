8f6710f1f6b732b7885874024123cc5c780303fd
Thabang Mokgonyana <thabangmokgonyana@gmail.com>
2026-03-31 12:49:31 +0200

    Created an index method for about controller
    Signed-off-by: Thabang Mokgonyana <thabangmokgonyana@gmail.com>

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class AboutController extends Controller
 {
-    //
+    public function index()
+    {
+        $companyName = 'My Company';
+        $description = 'We are a leading company in our industry, providing top-notch services to our clients. Our mission is to deliver exceptional value and exceed customer expectations.';
+        return view('about', [
+            'companyName' => $companyName,
+            'description' => $description,
+        ]);
+    }
 }
